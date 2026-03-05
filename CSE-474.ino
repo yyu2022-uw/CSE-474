974b3e6cb34f1b0aed2f9dce51611df95f78334d
Giannah Donahoe <152745136+GiannahD@users.noreply.github.com>
2026-03-04 23:34:06 -0800

    Sensor Reading
    - Define and setup sensors: water level, DHT11, motion, sound
    - Created Sensor Tasks to Core 0
    - Notify Sensor Value Readings within sensorTimeInterrupt

--- CSE-474.ino
+++ CSE-474.ino
@@ -8,6 +8,7 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include "esp_timer.h"
+#include <DHT.h>
 
 // ================ Macros ================
 #define SDA_PIN 8
@@ -17,6 +18,16 @@
 #define SENSOR_TIMER_INTERVAL 1000000
 #define MESSAGE_TIMER_INTERVAL 1000000
 #define DEBOUNCE_DELAY 200
+
+//sensors
+#define DHT11_PIN 4
+#define DHTTYPE DHT11
+#define WATER_POWER_PIN 17
+#define WATER_SIGNAL_PIN 36
+#define SOUND_PIN 18
+#define MOTION_PIN 27
+
+DHT dht(DHT11_PIN, DHTTYPE);
 
 // Generate random Service and Characteristic UUIDs: https://www.uuidgenerator.net/
 #define SERVICE_UUID        "2405162b-b220-47e6-a767-cc5d9437ccea"
@@ -28,6 +39,11 @@ esp_timer_handle_t sensor_timer;
 esp_timer_handle_t message_timer;
 volatile unsigned long lastWindowInterruptTime = 0;
 volatile unsigned long lastFanInterruptTime = 0;
+
+TaskHandle_t heatTaskHandle = nullptr;
+TaskHandle_t waterTaskHandle = nullptr;
+TaskHandle_t soundTaskHandle = nullptr;
+TaskHandle_t motionTaskHandle = nullptr;
 
 
 // ================ Functions ================
@@ -57,6 +73,10 @@ void IRAM_ATTR handleFanButtonInterrupt() {
 
 void IRAM_ATTR sensorTimerInterrupt(void* arg) {
   // Call xTaskNotify() to notify sensor value reading
+    vTaskNotifyGive(heatTaskHandle);
+    vTaskNotifyGive(waterTaskHandle);
+    vTaskNotifyGive(soundTaskHandle);
+    vTaskNotifyGive(motionTaskHandle);
 }
 
 void IRAM_ATTR messageTimerInterrupt(void* arg) {
@@ -88,6 +108,18 @@ void setup() {
   Wire.begin();
   lcd.init();
 
+  // Sensors
+  pinMode(WATER_POWER_PIN, OUTPUT);
+  pinMode(SOUND_PIN, INPUT);
+  pinMode(MOTION_PIN, INPUT);
+  dht.begin();
+
+  // Create Sensor Tasks
+  xTaskCreatePinnedToCore(heatTask, "TaskHeat", 4096, NULL, 1, &heatTaskHandle, 0);
+  xTaskCreatePinnedToCore(waterTask, "TaskWater", 4096, NULL, 1, &waterTaskHandle, 0);
+  xTaskCreatePinnedToCore(soundTask, "TaskSound", 4096, NULL, 1, &soundTaskHandle, 0);
+  xTaskCreatePinnedToCore(motionTask, "TaskMotion", 4096, NULL, 1, &motionTaskHandle, 0);
+
   // SENSOR TIMER
   esp_timer_create_args_t sensor_timer_args = {
     .callback = &sensorTimerInterrupt,
@@ -111,11 +143,55 @@ void setup() {
   // BUTTON2
   pinMode(FAN_BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(FAN_BUTTON_PIN), &handleFanButtonInterrupt, FALLING);
-  
-
 }
 
 void loop() {
   // FreeRTOS handles everything
 
+}
+
+void waterTask(void* pvParameters){
+  while(1){
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    digitalWrite(WATER_POWER_PIN, HIGH);
+    float value = analogRead(WATER_SIGNAL_PIN);
+    digitalWrite(WATER_POWER_PIN, LOW);
+  }
+}
+
+// void humdityTask(void* pvParameters){
+//   while(1){
+//     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+//     float h = dht.readHumidity();
+//   }
+// }
+
+// void tempTask(void* pvParameters){
+//   while(1){
+//     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+//     float t = dht.readTemperature(true);
+//   }
+// }
+
+void heatTask(void* pvParameters){
+  while(1){
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    float h = dht.readHumidity();
+    float t = dht.readTemperature(true);
+    float hi = dht.computeHeatIndex(t, h);
+  }
+}
+
+void soundTask(void* pvParameters){
+  while(1){
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    float s = digitalRead(SOUND_PIN);
+  }
+}
+
+void motionTask(void* pvParameters){
+  while(1){
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    float m = digitalRead(MOTION_PIN);
+  }
 }
